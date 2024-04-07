75be39132c0aa5651a908c90ff1bb5971491e5ca
Evgeniy Tokarev <tokarev007007@gmail.com>
2024-04-07 21:26:27 +0300

    add Prac 6 and 7

---
+++ Prac7/app/src/main/java/com/example/prac7/MusicService.java
@@ -0,0 +1,43 @@
+package com.example.prac7;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MusicService extends Service {
+    private static final String TAG = "MusicService";
+    private MediaPlayer mediaPlayer;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Инициализация медиаплеера
+        mediaPlayer = MediaPlayer.create(this, R.raw.rickroll);
+        mediaPlayer.setLooping(true); // Зацикливание воспроизведения
+        mediaPlayer.setVolume(100, 100);
+
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int
+            startId) {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            Log.d(TAG, "Музыка начала играть");
+        }
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            Log.d(TAG, "Музыка остановлена и ресурсы освобождены");
+        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null; // Для сервисов без привязки возвращаем null
+    }
+}
